30338d1e986a71ff98c7eaa95105e234310d7dda
Gabri177 <forestcat177@gmail.com>
2024-08-19 22:58:30 +0200

    update

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -2,21 +2,33 @@
 
 cd /var/www/html
 
-
 if [ ! -f wp-config.php ]; then
     wp core download --allow-root
+fi
 
-    wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --allow-root
+wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb --skip-check --force --allow-root
 
-    wp core install --url=https://yugao --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
-
-    wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+if wp core is-installed --allow-root; then
+    echo "WordPress is already installed, updating configuration..."
 else
-    echo "WordPress is already installed, updating user and password..."
+    wp core install --url=https://yugao --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --allow-root
+fi
 
+if wp user get $WP_ADMIN_USER --field=ID --allow-root > /dev/null 2>&1; then
+    echo "Updating password for existing admin user $WP_ADMIN_USER..."
     wp user update $WP_ADMIN_USER --user_pass=$WP_ADMIN_PASSWORD --allow-root
-	
+else
+    echo "Admin user $WP_ADMIN_USER does not exist. Creating..."
+    wp user create $WP_ADMIN_USER $WP_ADMIN_EMAIL --role=administrator --user_pass=$WP_ADMIN_PASSWORD --allow-root
+fi
+
+
+if wp user get $WP_USER --field=ID --allow-root > /dev/null 2>&1; then
+    echo "Updating password for existing user $WP_USER..."
     wp user update $WP_USER --user_pass=$WP_USER_PASSWORD --allow-root
+else
+    echo "Creating new user $WP_USER..."
+    wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 fi
 
 /usr/sbin/php-fpm7.3 -F -R
